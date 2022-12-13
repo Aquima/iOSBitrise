235060b616362314508ac3e4af30a3e217d76831
Raul Quispe <ext_raquispe@falabella.com>
2022-12-13 02:19:23 -0500

    Initial Commit

---
+++ iOSBitrise/iOSBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSBitriseApp.swift
+//  iOSBitrise
+//
+//  Created by Raul on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
